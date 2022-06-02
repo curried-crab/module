2346b013624e5c451865e024f624816ce3e03b9f
newrankyangjingyu <yjy15680489038@163.com>
2022-06-02 11:45:03 +0800

    feat: add img

--- require.js
+++ require.js
@@ -48,58 +48,47 @@
 
     // 查看是否有缓存
     if (moduleCache[modName]) {
-
-    } else {
-
-    }
-  }
-
-  var loadMod = function (modName, callback) {
-    var url = getPathUrl(modName), fs, mod;
-
-    if (moduleCache[modName]) {
-      mod = moduleCache[modName];
-      if (mod.status == 'loaded') {
-        setTimeout(callback(params), 0);
+      mod = moduleCache[modName]
+      if (mod.status === "loaded") {
+        setTimeout(callback(params), 0)
       } else {
-        mod.onload.push(callback);
+        mod.onload.push(callback)
       }
     } else {
       mod = moduleCache[modName] = {
-        modName: modName,
-        status: 'loading',
+        modName,
+        status: "loading",
         export: null,
         onload: [callback]
-      };
+      }
 
-      _script = document.createElement('script');
-      _script.id = modName;
-      _script.type = 'text/javascript';
-      _script.charset = 'utf-8';
-      _script.async = true;
-      _script.src = url;
+      const script = document.createElement("script")
+      script.id = modName
+      script.type = 'text/javascript';
+      script.charset = "utf-8"
+      script.async = true
+      script.src = url
 
-      fs = document.getElementsByTagName('script')[0];
-      fs.parentNode.insertBefore(_script, fs);
+      fs = document.getElementsByTagName("script")[0]
+      fs.parentNode.insertBefore(script, fs)
     }
-  };
+  }
 
-  var saveModule = function (modName, params, callback) {
-    var mod, fn;
-
-    if (moduleCache.hasOwnProperty(modName)) {
-      mod = moduleCache[modName];
-      mod.status = 'loaded';
+  function saveModule(modName, params, callback) {
+    let mod, fn;
+    if (moduleCache[modName]) {
+      mod = moduleCache[modName]
+      mod.status = "loaded"
 
       mod.export = callback ? callback(params) : null;
 
       while (fn = mod.onload.shift()) {
-        fn(mod.export);
+        fn(mod.export)
       }
     } else {
-      callback && callback.apply(window, params);
+      callback && callback.apply(window, params)
     }
-  };
+  }
 
   window.require = require;
   window.define = require;
